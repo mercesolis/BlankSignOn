c909914fc95ef62d7c0ea853919a7f1457e33251
mercesolis <mersolis@live.com>
2020-09-19 22:29:18 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'signin', component: SignInComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'userprofile', component: UserProfileComponent},
+  {path: '**', redirectTo: 'signin', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
